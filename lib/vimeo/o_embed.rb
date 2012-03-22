65ffd1e8c509a1d6e97ccc35d20d33f8ba915df2
Moritz Kröger <moritz.kroeger@googlemail.com>
2012-03-22 19:46:03 +0100

    added support for oembed query parameters

--- lib/vimeo/o_embed.rb
+++ lib/vimeo/o_embed.rb
@@ -3,8 +3,10 @@ module Vimeo
     include HTTParty
     base_uri 'vimeo.com/api'
 
-    def self.get_info(video_id)
-      get("/oembed.json?url=http%3A//vimeo.com/#{video_id}")
+    def self.get_info(video_id, params=nil)
+      query_url = "/oembed.json?url=http%3A//vimeo.com/#{video_id}"
+      query_url += "&"+params.to_query if params.present?
+      get(query_url)
     end
   end
 end
