9c3e050cec635575f8c1d69b11ff0b80bc50b0c4
Ramon Tayag <ramon@tayag.net>
2012-03-25 11:05:11 +0800

    Removes `.present?` call - probably an activesupport core_ext addition (something not present here)

--- lib/vimeo/o_embed.rb
+++ lib/vimeo/o_embed.rb
@@ -5,7 +5,7 @@ module Vimeo
 
     def self.get_info(video_id, params=nil)
       query_url = "/oembed.json?url=http%3A//vimeo.com/#{video_id}"
-      query_url += "&"+params.to_query if params.present?
+      query_url += "&"+params.to_query if params
       get(query_url)
     end
   end
